6df37cafe0c64bdd9f64eca021830617392acad3
gokhanyanik <gokhanyanik@gmail.com>
2023-11-16 16:09:49 +0300

    TouchableHighlight Component Example

--- App.tsx
+++ App.tsx
@@ -16,6 +16,8 @@ import {
   useColorScheme,
   View,
   Image,
+  TouchableHighlight,
+  TouchableOpacity,
 } from 'react-native';
 
 import {
@@ -84,12 +86,19 @@ function App(): JSX.Element {
       <Image
       source={{uri:"https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltdc07f051caa5cdf6/625adf7b956c474ead99e528/DepoPhotos_14812567_(1).jpg?auto=webp&format=pjpg&width=640&quality=60",
               width:400,
-              height:600,
+              height:100,
               }}
               resizeMode='contain'
               fadeDuration={6000}
               
       />
+      <TouchableHighlight onPress={()=> console.log("Open eyes!!")} >
+      <View style={{width:'100%', height:75, backgroundColor:'cyan'}}></View>
+      </TouchableHighlight>
+      <TouchableOpacity onPress={()=> console.log("reactnative!!")} >
+      <View style={{width:'100%', height:75, backgroundColor:'red'}}></View>
+      </TouchableOpacity>
+      
     </SafeAreaView>
   );
   
