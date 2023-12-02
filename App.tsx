396a6b2bf4c71d4e0f2d152818fff01a012ea5a7
gokhanyanik <gokhanyanik@gmail.com>
2023-12-02 23:41:01 +0300

    VS CODE Cleanup For Exercises

--- App.tsx
+++ App.tsx
@@ -4,19 +4,13 @@ import {
   StyleSheet,
   Text,
   Platform,
-  Dimensions,
 } from 'react-native';
 
 function App(): JSX.Element {
   return (
-    console.log(Platform.OS,Dimensions.get("screen")),
     <SafeAreaView style={styles.background}>
-      <Text style={[styles.text,styles.metin]}> Bu bir {Platform.OS} uygulamasidir </Text>
-      <Text style={styles.text}>Height:{Dimensions.get("window").height}</Text>
-      <Text style={styles.text}>Widht:{Dimensions.get("window").width}</Text>
-      <Text style={styles.text}>Height:{Dimensions.get("screen").height}</Text>
-      <Text style={styles.text}>Widht:{Dimensions.get("screen").width}</Text>
- 
+      
+      
     </SafeAreaView>
   );
 }
@@ -28,18 +22,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    color: 'white',
-    fontSize: 25
-  },
-  metin:{
-    backgroundColor:'green'
-  
-  },
-  metin2:{
-    color:'red',
-    fontSize:30
-  }
+ 
   
 });
 
