71075b7e3cc6a581562e8bb8497fe78809c53e8a
gokhanyanik <gokhanyanik@gmail.com>
2023-12-08 20:43:29 +0300

    Card Project Completed

--- App.tsx
+++ App.tsx
@@ -13,13 +13,27 @@ function App(): JSX.Element {
     <View style={styles.container}>
     <View style={styles.top}>
      <View style={styles.middle}>
-       <View style={styles.bottom}/>
        <View style={styles.bottom}>
-         <View style={styles.middle}/>
-         <View style={styles.middle}/>
+       <Image
+         source={{uri:'https://minio.yalispor.com.tr/yalispor/blog/alex-de-souza-kimdir-1_606ee18018d64.jpg'}}
+         style={{height:250,width:200,alignItems:'flex-start'}}
+       /> 
+       </View>
+       <View style={styles.bottom}>
+         <View style={styles.middle}>
+             <Text style={{textAlign:'center',fontWeight:'bold'}}>ALEX DE SOUZA</Text>
+         </View>
+         <View style={styles.middle}>
+         <Text numberOfLines={2} style={{alignItems:'center'}}>For stretch to have an effect, children must not have a fixed dimension along the secondary axis.</Text>
+         </View>
        </View>
      </View>
-     <View style={styles.middle}/>
+     <View style={styles.middle}>
+       <Text>
+       For stretch to have an effect, children must not have a fixed dimension along the secondary axis. In the following example, setting alignItems: stretch does nothing until the width: 50 is removed from the children.
+       </Text>
+     
+     </View>
     </View>
   </View>
   );
@@ -30,29 +44,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     padding: 20,
     margin: 10,
-    borderWidth:5
-    
-  },
-  top: {
+ },
+   top: {
     flex: 0.8,
     backgroundColor: 'grey',
-    borderWidth: 5,
-   
-  },
-  middle: {
+ },
+   middle: {
     flex: 0.5,
-    backgroundColor: 'beige',
-    borderWidth: 3,
+    backgroundColor: 'white',
     margin: 10,
     flexDirection:'row'
-    
-  },
-  bottom: {
+ },
+   bottom: {
     flex: 0.5,
-    backgroundColor: 'pink',
-    borderWidth: 3,
-    
-  },
+    backgroundColor: 'white',
+ },
 });
 
 export default App;
