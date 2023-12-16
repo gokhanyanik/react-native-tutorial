17e63b4220a9b2be226b6d4b549b7b7fb98adc33
gokhanyanik <gokhanyanik@gmail.com>
2023-12-16 21:30:51 +0300

    zIndex Example

--- App.tsx
+++ App.tsx
@@ -13,16 +13,14 @@ function App(): JSX.Element {
   return (
     <SafeAreaView style={styles.container}>
 
-       <View style={{height:100,width:75,backgroundColor:'purple'}}/>
-       <View style={{height:100,width:75,backgroundColor:'yellow',bottom:20,left:20,position:'absolute'}}/>
-       <View style={{height:100,width:75,backgroundColor:'orange'}}/>
-       
-      
-
-
-
+       <View style={{height:100,width:75,backgroundColor:'purple',position:'absolute',top:40,left:40,zIndex:4}}/> 
+       <View style={{height:100,width:75,backgroundColor:'yellow',position:'absolute',top:60,left:60,zIndex:2}}/>
+       <View style={{height:100,width:75,backgroundColor:'orange',position:'absolute',top:80,left:80,zIndex:3}}/>
+ 
     </SafeAreaView>
-  );
+//zIndex arka arkaya siralanan viwlerin siralamasini degistirmek icin kullanilir.
+//normalde en altta gorunen viwi en ustte gormek icin zindex degerini 1 veririz.
+//bir kac tane zindex birlikte kullanilirsa en buyuk deger en ustte olur sayi arttikca siralamada ilk sirada gosterilir.  
 }
 const styles = StyleSheet.create({
   container: {
