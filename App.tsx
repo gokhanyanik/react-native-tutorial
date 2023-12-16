fddf132a25845680bac41835e9e997fd7c6ca74b
gokhanyanik <gokhanyanik@gmail.com>
2023-12-16 10:38:26 +0300

    Flex Example

--- App.tsx
+++ App.tsx
@@ -11,20 +11,22 @@ import Card from './components/Card'
 
 function App(): JSX.Element {
   return (
-    <View style={styles.container}>
-      <Card star={'TADİÇ'} aciklama={'tadiç on fire'} paragraf={'tadiç tadiç tadi. tadiç'} resim="https://tmssl.akamaized.net/images/foto/newsansicht/dusan-tadic-fenernahce-2023-24-1689579033-111706.jpg?lm=1689579051" />
-      <Card star={'DZEKO'} aciklama={' Boşnak millî futbolcudur. Forvet pozisyonunda görev yapan oyuncu, Süper Lig takımlarından Fenerbahçede forma giymektedir'} paragraf={'jeko jeko jeko jeko'} resim="https://cdn1.ntv.com.tr/gorsel/u1cvKpvuxUKsvkB2b-AjgQ.jpg?width=952&height=540&mode=both&scale=both" />
-      <Card star={'KAPTAN'} aciklama={'Dzeko büyük kaptan'} paragraf={'kaptan kaptan kaptan'} resim="https://pbs.twimg.com/media/FzPaAOrXwAkFIH1.jpg" />
-      <Card star={'İrfan Can Kahveci'} aciklama={'süper bilek'} paragraf={'kaptan kaptan kaptan'} resim="https://pbs.twimg.com/media/FzPaAOrXwAkFIH1.jpg" />
-    </View>
+    <SafeAreaView style={styles.container}>
+
+       <View style={{flex:1,backgroundColor:'purple'}}/>
+       <View style={{flex:2,backgroundColor:'yellow'}}/>
+       <View style={{flex:2,backgroundColor:'orange'}}/>
+
+
+
+    </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    padding: 5,
-    margin: 10,
+    backgroundColor: 'brown',
+    
   }
 });
 
