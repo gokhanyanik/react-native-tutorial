9bf578ed2dc18aa46462ff150ca5c18cb616aa26
gokhanyanik <gokhanyanik@gmail.com>
2023-12-16 22:18:59 +0300

    Orientation Example

--- App.tsx
+++ App.tsx
@@ -13,23 +13,21 @@ function App(): JSX.Element {
   return (
     <SafeAreaView style={styles.container}>
 
-       <View style={{height:100,width:75,backgroundColor:'purple',position:'absolute',top:40,left:40,zIndex:4}}/> 
-       <View style={{height:100,width:75,backgroundColor:'yellow',position:'absolute',top:60,left:60,zIndex:2}}/>
-       <View style={{height:100,width:75,backgroundColor:'orange',position:'absolute',top:80,left:80,zIndex:3}}/>
+       <View style={{flex:1,backgroundColor:'purple'}}/> 
+       <View style={{flex:1,backgroundColor:'yellow'}}/>
+       <View style={{flex:1,backgroundColor:'orange'}}/>
  
     </SafeAreaView>
-//zIndex arka arkaya siralanan viwlerin siralamasini degistirmek icin kullanilir.
-//normalde en altta gorunen viwi en ustte gormek icin zindex degerini 1 veririz.
-//bir kac tane zindex birlikte kullanilirsa en buyuk deger en ustte olur sayi arttikca siralamada ilk sirada gosterilir.  
+
+);
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'brown',
     //viwleri yatay sıralar.
-    flexDirection:'row',
-    justifyContent:'center',// elementleri main ekseninde ortalar.eger flexdirection row verildiyse y ekseninde yatay hizalıyacaktır.
-    alignItems:'center',
+   
+    
     
   }
 });
